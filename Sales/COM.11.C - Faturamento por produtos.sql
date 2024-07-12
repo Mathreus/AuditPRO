19290a7f5ccc16291c0becee72587533a223e88b
AnalyticAudit <112398318+Mathreus@users.noreply.github.com>
2024-07-12 14:20:20 -0300

    COM.11.C -  Product billing
    The analysis of sales revenue by products is crucial for effective sales and inventory management in a company. By evaluating the performance of each product in terms of generated revenue, it's possible to identify which items are the most profitable and popular among customers. This analysis not only aids in strategic decision-making, such as pricing adjustments and promotions, but also facilitates inventory planning and resource allocation. Additionally, it allows for identifying opportunities to launch new products or discontinue less profitable items, thereby maximizing return on investment.

---
+++ Sales/COM.11.C - Faturamento por produtos.sql
@@ -0,0 +1,59 @@
+SELECT DISTINCT
+	CONCAT(left(nf.bukrs, 2), right(nf.branch, 2)) as Centro,
+	NF.PSTDAT as Dt_lan,
+  PED.SPART as SetorAtividade,
+  NF.PARID as Id_Externo,
+	NF.NAME1 as Cliente,
+	PED.VBELN as Pedido,
+	NF.NFENUM as Num_nfe,
+  LIN.MATNR AS Codigo,
+  LIN.MAKTX as Texto_Breve_Material,
+  LIN.MENGE as Quantidade,  
+	LIN.NETWR as Faturamento,
+  PED.VKBUR as Equipe_Vendas,
+	RELV.PARTNER as Cod_orig_vend, 
+	CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) as Vendedor
+from
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak` as ped
+inner join  
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap` as item on
+  ped.mandt = item.mandt and
+  ped.vbeln = item.vbeln
+inner join 
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbfa` as fa on
+  ped.mandt = fa.mandt and 
+  ped.vbeln = fa.vbelv and
+  vbtyp_n = 'M'
+inner join
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbrk` as fat on
+  fat.mandt = ped.mandt and
+  fa.vbeln = fat.vbeln
+inner join
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbrp` as rp on
+  rp.mandt = ped.mandt and
+  fat.vbeln = rp.vbeln
+inner join
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` as lin on
+  lin.mandt = ped.mandt and
+  rp.vbeln = lin.refkey and
+  rp.matnr = lin.matnr
+inner join
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` as nf on
+  nf.mandt = ped.mandt and
+  lin.docnum = nf.docnum
+inner join 
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` as relv on
+  ped.mandt = relv.client and
+  relv.idnumber = item.perve_ana
+inner join
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` as vend on
+  ped.mandt = vend.client and
+  relv.partner = vend.partner
+WHERE
+  nf.mandt = '300' AND
+  nf.pstdat between '2024-01-01' and '2024-06-30' AND
+--  nf.parid > '1000000000' and
+  nf.parid = '1000194631' AND
+  nf.nfenum <> 'NULL' and
+  nf.cancel <> 'X' AND  
+	nf.direct = '2'
