d40213ba6ad63546de04c1db645bb15f2b4e8ff8
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2024-08-06 10:48:53 -0300

    FAT.10 - Sales order with Shipments created without invoicing
    Analyzing sales orders with shipments created but not invoiced is crucial for identifying operational issues, ensuring inventory and logistics control, managing cash flow, ensuring compliance with regulations, maintaining customer satisfaction, and preventing fraud. This analysis reveals failures in invoicing or delays in necessary documentation, ensures that products were shipped and received correctly, directly impacts the company's financial planning and cash flow, prevents issues with audits and regulatory bodies, guarantees that customers receive correct invoices promptly, and detects potential irregularities and fraud.

---
+++ Invoicing/FAT.10 - Ordem de vendas com Remessas criadas sem faturamento.sql
@@ -0,0 +1,57 @@
+SELECT DISTINCT
+  item.werks AS Centro,
+  ped.auart AS Documentos_Vendas, 
+  CASE
+    WHEN ped.spart = '01' THEN 'Consumo'
+    ELSE 'Revenda'
+  END SetorAtividade,
+  ped.LAST_CHANGED_BY_USER AS Usuario,
+  ped.kunnr AS ID_Externo, 
+  cli.name1 AS Cliente,
+  ped.erdat AS DataPedido,
+  rem.erdat AS DataRemessa,
+  RIGHT(ped.vbeln, 7) AS Pedido, 
+  REM.vbeln AS Remessa,
+  RIGHT(item.matnr, 6) AS CodigoMaterial, 
+  item.arktx AS Material,
+  item.kwmeng AS QTD_Ordem, 
+  item.netwr AS Valor_Pedido,
+  fa.vbeln as DOC_FAT,
+  RELV.PARTNER AS Cod_Vendedor, 
+  CONCAT(VEND.BU_SORT2, ' ', VEND.BU_SORT1) AS Vendedor  
+FROM
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak` AS ped
+INNER JOIN  
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap` AS item ON
+  ped.mandt = item.mandt AND
+  ped.vbeln = item.vbeln
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.lips` AS REM ON 
+  item.vbeln = rem.vgbel and
+  item.posnr = rem.vgpos
+LEFT JOIN 
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbfa` AS FA ON 
+  rem.vbeln = fa.vbelv AND 
+  rem.posnr = fa.posnv AND 
+  FA.vbtyp_n = 'M'
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.t001w` AS CEN ON
+  CEN.MANDT = REM.MANDT AND
+  CEN.WERKS = REM.WERKS
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1` as cli on
+  ped.mandt = cli.mandt and
+  ped.kunnr = cli.kunnr
+INNER JOIN 
+	`production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` as relv on
+	ped.mandt = relv.client and
+	relv.idnumber = item.perve_ana
+INNER JOIN
+`production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` as vend on
+	ped.mandt = vend.client and
+	relv.partner = vend.partner
+WHERE
+  fa.vbeln is null AND
+  ped.abstk = 'A' AND
+  ped.auart in ( 'OR1','ZCAS','ZPDV') AND
+  DATE_DIFF(CURRENT_DATE(), REM.ERDAT, DAY) > 3
