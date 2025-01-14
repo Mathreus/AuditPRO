00aa17b85ff3e98c7b14663fd135cf6c470a4e27
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-01-14 09:59:19 -0300

    EST.20 - Existence of long-standing receipt balances
    The analysis of long-standing goods receipt balances is essential for identifying potential inconsistencies in the receiving and accounting processes. These balances may indicate failures in cost recognition, undelivered goods, or operational backlogs. Reviewing them prevents negative cash flow impacts, mitigates fraud risks, and ensures greater compliance with internal and regulatory policies, fostering more efficient financial management.

---
+++ Stock/EST.20 - Existência de saldos no DREC a longa data.sql
@@ -0,0 +1,33 @@
+SELECT DISTINCT
+  MB.BWKEY AS Centro,
+  POS.ERSDA AS DataCriada,
+  POS.LGORT AS Deposito,
+  RIGHT(MB.MATNR, 6) AS Material,
+  TEX.MAKTX AS Texto_Breve_Material,
+--  M.MATKL AS Grupo_Mercadorias,
+--  MB.LBKUM AS QTD,
+--  MB.SALK3 AS Montante,
+--  POS.LABST AS Utilizacao_Livre,
+--  (POS.LABST * MB.VERPR) AS Val_utiliz_livre,
+--  POS.SPEME AS Bloqueado,
+--  (POS.SPEME * MB.VERPR) AS Val_estoque_bloq,
+  (POS.LABST + POS.SPEME) AS Quantidade,
+--  MB.VERPR AS Preco_Unitario,
+  ((POS.LABST + POS.SPEME) * MB.VERPR ) AS Valor_Estoque
+FROM
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.mbew` AS MB
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.makt` AS TEX ON
+  MB.MATNR = TEX.MATNR 
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.nsdm_v_mard` AS POS ON
+  MB.BWKEY = POS.WERKS 
+  AND MB.MATNR = POS.MATNR
+INNER JOIN  
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.mara` as M ON
+  MB.MATNR = M.MATNR 
+WHERE
+  POS.LGORT = 'DREC'
+  AND (POS.LABST + POS.SPEME) <> 0
+ORDER BY
+  MB.BWKEY ASC
