521007dde2bb0f7a142f674a39d48cb34d7ab232
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-03-18 11:26:29 -0300

    Update CR.11 - Prorrogação de títulos do contas a receber.sql

--- Financial/CR.11 - Prorrogação de títulos do contas a receber.sql
+++ Financial/CR.11 - Prorrogação de títulos do contas a receber.sql
@@ -1,45 +1,53 @@
 SELECT DISTINCT
   FIN.BUKRS AS COD_EMPRESA,
+  FIN.BUPLA AS LOCAL_NEG,
   EMP.BUTXT AS EMPRESA,
-  FIN.KUNNR as COD_CLIENTE,
+  FIN.KUNNR AS COD_CLIENTE,
   CLI.NAME1 AS CLIENTE,
-  FIN.BELNR as DOC_CONTABIL,
+  FIN.BELNR AS DOC_CONTABIL,
   FIN.BUZEI AS ITEM_CONTABIL,
-  FIN.DMBTR as VALOR_TIT,
+  FIN.DMBTR AS VALOR_TIT,
   FIN.SGTXT AS DESCRICAO,
   FIN.H_BLART AS COD_TIPO_DOCUMENTO_CONTABIL,
-  cdhdr.CHANGENR as DOC_ALTERACAO,
-  cdhdr.USERNAME as USUARIO,
-  cdpos.VALUE_OLD as VALOR_ANTIGO,
-  cdpos.VALUE_NEW as VALOR_NOVO,
-  cdhdr.UDATE as DT_ALTERACAO,
-  cdhdr.TCODE as TRANSACAO,
-  cdhdr.CHANGE_IND as TP_MODIF,
-  CASE WHEN cdhdr.CHANGE_IND = 'U' THEN 'ATUALIZACAO' 
-       WHEN cdhdr.CHANGE_IND = 'I' THEN 'INSERIDO' 
-       WHEN cdhdr.CHANGE_IND = 'D' THEN 'DELETADO' END AS TP_ALTERACAO, 
-  cdpos.FNAME as CAMPO_ALTERADO,
-  'DATA BASE PARA PAGAMENTO' as DESCRICAO_CAMPO,
-  FIN.BUPLA AS LOCAL_NEG,
-  FIN.H_BUDAT as DT_LANCAMENTO,
-  FIN.AUGDT as DT_COMPENSACAO,
-  FIN.NETDT as DT_VENCIMENTO,
-  FIN.ZLSCH as FORM_PAG,
-  FIN.HBKID as BANCO
+  cdhdr.CHANGENR AS DOC_ALTERACAO,
+  cdhdr.USERNAME AS USUARIO,
+  FORMAT_DATE('%d/%m/%Y', PARSE_DATE('%Y%m%d', cdpos.VALUE_OLD)) AS VENCIMENTO_ANTIGO,
+  FORMAT_DATE('%d/%m/%Y', PARSE_DATE('%Y%m%d', cdpos.VALUE_NEW)) AS VENCIMENTO_NOVO,
+  DATE_DIFF(PARSE_DATE('%Y%m%d', cdpos.VALUE_NEW), PARSE_DATE('%Y%m%d', cdpos.VALUE_OLD), DAY) AS DIF_DIAS,
+  cdhdr.UDATE AS DT_ALTERACAO,
+  cdhdr.TCODE AS TRANSACAO,
+  cdhdr.CHANGE_IND AS TP_MODIF,
+  CASE 
+    WHEN cdhdr.CHANGE_IND = 'U' THEN 'ATUALIZACAO' 
+    WHEN cdhdr.CHANGE_IND = 'I' THEN 'INSERIDO' 
+    WHEN cdhdr.CHANGE_IND = 'D' THEN 'DELETADO' 
+  END AS TP_ALTERACAO, 
+  cdpos.FNAME AS CAMPO_ALTERADO,
+  'DATA BASE PARA PAGAMENTO' AS DESCRICAO_CAMPO,
+  FIN.H_BUDAT AS DT_LANCAMENTO,
+  FIN.AUGDT AS DT_COMPENSACAO,
+  FIN.NETDT AS DT_VENCIMENTO,
+  FIN.ZLSCH AS FORM_PAG,
+  VEN.CNAME AS VENDEDOR
 FROM		
-	production-servers-magnumtires.prdmgm_sap_cdc_processed.bseg AS FIN
+  production-servers-magnumtires.prdmgm_sap_cdc_processed.bseg AS FIN
 INNER JOIN 
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.cdpos ON CONCAT(FIN.BUKRS,FIN.BELNR, FIN.GJAHR, FIN.BUZEI) = RIGHT(cdpos.TABKEY,21)
+  production-servers-magnumtires.prdmgm_sap_cdc_processed.cdpos 
+  ON CONCAT(FIN.BUKRS, FIN.BELNR, FIN.GJAHR, FIN.BUZEI) = RIGHT(cdpos.TABKEY, 21)
 INNER JOIN
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.cdhdr ON cdhdr.CHANGENR = cdpos.CHANGENR 
+  production-servers-magnumtires.prdmgm_sap_cdc_processed.cdhdr 
+  ON cdhdr.CHANGENR = cdpos.CHANGENR 
 INNER JOIN 
-  `production-servers-magnumtires.prdmgm_sap_cdc_processed.t001` AS EMP ON 
-  EMP.BUKRS = FIN.BUKRS
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.t001` AS EMP 
+  ON EMP.BUKRS = FIN.BUKRS
 INNER JOIN 
-  `production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1` AS CLI ON
-  CLI.KUNNR = FIN.KUNNR
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1` AS CLI 
+  ON CLI.KUNNR = FIN.KUNNR
+INNER JOIN  
+  `Portal_do_Vendedor.Clientes_x_Vendedores` AS VEN
+  ON CLI.KUNNR = VEN.KUNNR
 WHERE
   FIN.SHKZG = 'S'
   AND cdpos.FNAME = 'ZFBDT'
   AND cdpos.TABNAME = 'BSEG'
-  AND cdhdr.udate BETWEEN '2024-07-01' AND '2024-07-31'
+  AND cdhdr.UDATE > '2022-01-01'
