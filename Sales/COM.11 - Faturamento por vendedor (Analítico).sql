fd46cef54a401ba8ac265a428e7cc6c3388f91b6
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-05-12 09:02:35 -0300

    Update COM.11 - Faturamento por vendedor (Analítico).sql

--- Sales/COM.11 - Faturamento por vendedor (Analítico).sql
+++ Sales/COM.11 - Faturamento por vendedor (Analítico).sql
@@ -1,61 +1,64 @@
-
 SELECT DISTINCT
 	CONCAT(left(nf.bukrs, 2), right(nf.branch, 2)) as Centro,
 	NF.PSTDAT as Dt_lan,
   CASE
     WHEN ped.spart = '01' THEN 'Consumo'
-    ELSE 'Revenda'
-  END SetorAtividade,
-  NF.PARID as Id_Externo,
-  NF.NAME1 as Cliente,
-  PED.VBELN as Pedido,
-  NF.NFENUM as Num_nfe,  
-  NF.NFTOT as Faturamento,
-  NF.NATOP AS Referencia,
-  PED.VKBUR as Equipe_Vendas,
-  RELV.PARTNER as Cod_orig_vend, 
-  CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) as Vendedor
+    WHEN ped.spart = '02' THEN 'Revenda' 
+    ELSE 'Nada'
+  END Canal,
+  NF.PARID as ID_Externo,
+	NF.NAME1 as Cliente,
+	PED.VBELN as Pedido,
+	NF.NFENUM as Num_nfe,  
+	NF.NFTOT as Faturamento,
+--  NF.NATOP AS Referencia,
+  PED.VKGRP AS Equipe_Vendas,
+  PED.VKBUR as Escritorio_Vendas,
+	RELV.PARTNER as Cod_orig_vend, 
+	CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) as Vendedor
 FROM
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak` as ped
-inner join  
+INNER JOIN  
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap` as item on
   ped.mandt = item.mandt and
   ped.vbeln = item.vbeln
-inner join 
+INNER JOIN 
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbfa` as fa on
   ped.mandt = fa.mandt and 
   ped.vbeln = fa.vbelv and
   vbtyp_n = 'M'
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbrk` as fat on
   fat.mandt = ped.mandt and
   fa.vbeln = fat.vbeln
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbrp` as rp on
   rp.mandt = ped.mandt and
   fat.vbeln = rp.vbeln
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` as lin on
   lin.mandt = ped.mandt and
   rp.vbeln = lin.refkey and
   rp.matnr = lin.matnr
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` as nf on
   nf.mandt = ped.mandt and
   lin.docnum = nf.docnum
-inner join 
+INNER JOIN 
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` as relv on
   ped.mandt = relv.client and
   relv.idnumber = item.perve_ana
-inner join
-  `production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` as vend on
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` as vend ON
   ped.mandt = vend.client and
   relv.partner = vend.partner
 WHERE
-  nf.mandt = '300' AND
-  nf.pstdat between '2025-03-01' and '2025-03-31' AND
-  nf.parid > '1000000000' and
-  nf.nfenum <> 'NULL' and
-  nf.cancel <> 'X' AND  
-  nf.direct = '2' AND
-  nf.natop LIKE '%Vnd.mer.adq%'
+--  CONCAT(left(nf.bukrs, 2), right(nf.branch, 2)) = '2015' AND
+  nf.pstdat BETWEEN '2025-04-01' and '2025-04-30' 
+  AND nf.parid > '1000000000'
+  AND nf.nfenum <> 'NULL' 
+  AND nf.cancel <> 'X'   
+	AND nf.direct = '2' 
+  AND nf.nftype = 'YC'
+--  AND CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) LIKE '%%'
+--  AND NF.NAME1 LIKE '%%'
