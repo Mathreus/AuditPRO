12d565955ec6d7b81ff03294075ddd1417369f7f
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-05-22 11:13:39 -0300

    Update FAT.14 - Volume de cancelamento de vendas casadas (ZCAS).sql

--- Invoicing/FAT.14 - Volume de cancelamento de vendas casadas (ZCAS).sql
+++ Invoicing/FAT.14 - Volume de cancelamento de vendas casadas (ZCAS).sql
@@ -1,48 +1,48 @@
 SELECT DISTINCT
-	CONCAT(left(nf.bukrs, 2), right(nf.branch, 2)) as Centro,
+	LIN.BWKEY as Centro,
 	NF.PSTDAT as Dt_lan,
-  CASE
-    WHEN ped.spart = '01' THEN 'Consumo'
-    WHEN ped.spart = '02' THEN 'Revenda'
-    ELSE 'Não Identificado'
-  END Canal,
-  NF.PARID as ID_Externo,
+  	CASE
+    		WHEN ped.spart = '01' THEN 'Consumo'
+    		WHEN ped.spart = '02' THEN 'Revenda'
+    		ELSE 'Não Identificado'
+  	END Canal,
+  	NF.PARID as ID_Externo,
 	NF.NAME1 as Cliente,
 	PED.VBELN as Pedido,
-  CASE
-    WHEN PED.ABSTK = 'A' THEN 'Pedido Aprovado'
-    WHEN PED.ABSTK = 'B' THEN 'Pedido aprovado parcialmente'
-    WHEN PED.ABSTK = 'C' THEN 'Pedido Cancelado'
-  END Aprovacao_Pedido,
+  	CASE
+    		WHEN PED.ABSTK = 'A' THEN 'Pedido Aprovado'
+    		WHEN PED.ABSTK = 'B' THEN 'Pedido aprovado parcialmente'
+    		WHEN PED.ABSTK = 'C' THEN 'Pedido Cancelado'
+  	END Aprovacao_Pedido,
 	NF.NFENUM as Num_nfe,
-  NF.CANCEL as Estorno,
-  NF.CRETIM AS Horario,
-  NF.CHATIM AS Hora_Modif,
-  NF.CRENAM as Usuario,  
+  	NF.CANCEL as Estorno,
+  	NF.CRETIM AS Horario,
+  	NF.CHATIM AS Hora_Modif,
+  	NF.CRENAM as Usuario,  
 	NF.NFTOT as Faturamento,
-  NF.NATOP AS Referencia,
-  PED.VKBUR as Equipe_Vendas,
+  	NF.NATOP AS Referencia,
+ 	 PED.VKBUR as Equipe_Vendas,
 	RELV.PARTNER as Cod_orig_vend, 
 	CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) as Vendedor
 FROM
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak as ped
+  	production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak as ped
 INNER JOIN 
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap as item on
-  ped.mandt = item.mandt and
-  ped.vbeln = item.vbeln
+  	production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap as item on
+  	ped.mandt = item.mandt and
+  	ped.vbeln = item.vbeln
 INNER JOIN
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin as lin on
-  lin.xped = item.vbeln and
-  lin.matnr = item.matnr and
-  lin.itmnum = item.posnr 
+  	production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin as lin on
+  	lin.xped = item.vbeln and
+  	lin.matnr = item.matnr and
+  	lin.itmnum = item.posnr 
 INNER JOIN
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc as nf on
-  lin.mandt = nf.mandt and
-  lin.docnum = nf.docnum
+ 	production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc as nf on
+  	lin.mandt = nf.mandt and
+  	lin.docnum = nf.docnum
 INNER JOIN
-  production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1 as cli on
-  ped.mandt = cli.mandt and
-  ped.kunnr = cli.kunnr
+  	production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1 as cli on
+  	ped.mandt = cli.mandt and
+  	ped.kunnr = cli.kunnr
 INNER JOIN 
 	production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id as relv on
 	ped.mandt = relv.client and
