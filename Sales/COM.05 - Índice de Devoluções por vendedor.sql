462b694b954d87a68a4071ccda930edb76ed7104
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-05-22 11:10:26 -0300

    Update COM.05 - Índice de Devoluções por vendedor.sql

--- Sales/COM.05 - Índice de Devoluções por vendedor.sql
+++ Sales/COM.05 - Índice de Devoluções por vendedor.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
-	concat(left(nf.bukrs, 2), right(nf.branch, 2)) as Centro,
-  nf.PSTDAT as Dt_Lan,
+	LIN.BWKEY as Centro,
+  	nf.PSTDAT as Dt_Lan,
 	nf.parid as ID_Externo,
 	nf.name1 as Cliente,
 	ped.vbeln as Pedido,
@@ -9,36 +9,36 @@ SELECT DISTINCT
 	ped.vkbur as EscrVen,
 	relv.partner as cod_orig_vend, 
 	concat(vend.bu_sort2,' ',vend.bu_sort1) as Vendedor
-from
+FROM
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak` as ped
-inner join  
+INNER JOIN  
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbap` as item on
   ped.mandt = item.mandt and
   ped.vbeln = item.vbeln
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` as lin on
   lin.xped = item.vbeln and
   lin.matnr = item.matnr and
   lin.itmnum = item.posnr 
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` as nf on
   lin.mandt = nf.mandt and
   lin.docnum = nf.docnum
-inner join
+INNER JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.kna1` as cli on
   ped.mandt = cli.mandt and
   ped.kunnr = cli.kunnr
-inner join 
+INNER JOIN 
 	`production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` as relv on
 	ped.mandt = relv.client and
 	relv.idnumber = item.perve_ana
-inner join
+INNER JOIN
 	`production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` as vend on
 	ped.mandt = vend.client and
 	relv.partner = vend.partner
-where
+WHERE
 	nf.pstdat between '2025-03-01' and '2024-05-31' and
-  nf.parid > '1000000000' and
+  	nf.parid > '1000000000' and
 	ped.vbeln like '%006%' and
 	nf.cancel <> 'X' and 
 	nf.nfenum <> 'NULL' and  
