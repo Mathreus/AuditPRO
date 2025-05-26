b226cf7d5314fa701da9855225a71b27271ee69e
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-05-26 14:35:41 -0300

    Update COM.41 - Levantamento de custo de pedidos.sql

--- Sales/COM.41 - Levantamento de custo de pedidos.sql
+++ Sales/COM.41 - Levantamento de custo de pedidos.sql
@@ -1,13 +1,17 @@
 SELECT DISTINCT
-  item.werks AS Centro,
-  ped.auart AS Documentos_Vendas, 
-  ped.erdat AS Data_Pedido,
-  ped.AEDAT AS Data_Cancelamento,
-  ped.kunnr AS ID_Externo, 
-  C.name1 AS Cliente,
+  ITEM.WERKS AS Centro,
+  PED.AUART AS Documentos_Vendas, 
+  PED.KUNNR AS ID_Externo, 
+  C.NAME1 AS Cliente,
   PED.VBELN AS Pedido,
-  PED.netwr AS Valor_Cancelamento,
-  RELV.PARTNER AS Cod_orig_vend, 
+  PED.ERDAT AS Data_Pedido,
+  EXTRACT(YEAR FROM ped.erdat) AS Ano,
+  CASE 
+    WHEN EXTRACT(YEAR FROM ped.erdat) = 2024 THEN '3'
+    WHEN EXTRACT(YEAR FROM ped.erdat) = 2025 THEN '5'
+    ELSE '0'
+  END Custo_Pedido,
+  PED.NETWR AS Montante_Pedido, 
   CONCAT(VEND.BU_SORT2,' ',VEND.BU_SORT1) AS Vendedor  
 FROM
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.vbak` AS PED
@@ -28,5 +32,6 @@ INNER JOIN
   AND relv.partner = vend.partner
 WHERE
   PED.ERDAT BETWEEN '2024-01-01' AND '2025-04-30'
+  AND PED.AUART IN ('OR1', 'ZPDV', 'ZCAS') 
   AND PED.ABSTK = 'C'
   AND PED.AUART IN ('OR1', 'ZPDV', 'ZCAS') 
