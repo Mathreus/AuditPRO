f0d55547e3cef2f6de895ca2f5fc18014a711325
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-06-13 11:06:10 -0300

    COM.40 - Occurrence of returns after closing
    Auditing the occurrence of returns after period-end closing is critical to ensure accurate financial reporting and compliance with revenue recognition standards. Failure to properly account for post-closing returns can lead to material misstatements, overstated revenue, and regulatory scrutiny. Additionally, excessive or unrecorded returns may indicate underlying issues such as product quality problems, sales channel disputes, or even fraudulent revenue inflation. Auditors must assess whether returns were properly estimated, accrued, and disclosed, as unrecorded liabilities can distort financial health and mislead stakeholders. Proactive monitoring of post-period returns helps mitigate risks of restatements and reputational damage.

---
+++ Sales/COM.40 - Ocorrência de devoluções após fechamento.sql
@@ -0,0 +1,32 @@
+SELECT DISTINCT
+  LIN.BWKEY AS Centro,
+  NF.PSTDAT AS Data_NF,
+  NF.PARID AS ID_Externo,
+  NF.NAME1 AS Cliente,
+  NF.NFENUM AS NF_Faturada,
+  RIGHT(LIN.MATNR, 6) AS Material_Faturamento,
+  LIN.MAKTX AS Descricao_Material_Faturamento,
+  NF.NFTOT AS Valor_Faturamento,
+  DEV.PSTDAT AS Data_Devolucao,
+  DEV.NFENUM AS NF_Devolucao,
+  RIGHT(DEVLIN.MATNR, 6) AS Material_Devolvido,
+  DEVLIN.MAKTX AS Descricao_Material_Devolvido,
+  DEV.NFTOT AS Valor_Devolucao
+FROM    
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` AS NF
+INNER JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` AS LIN
+  ON NF.DOCNUM = LIN.DOCNUM 
+LEFT JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` AS DEV
+  ON NF.PARID = DEV.PARID  -- Mesmos clientes
+  AND NF.NAME1 = DEV.NAME1 -- Mesmo nome de cliente
+  AND DEV.NFTYPE IN ('ZD', 'Y2') -- Tipos de nota fiscal que indicam devolução
+LEFT JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` AS DEVLIN
+  ON DEV.DOCNUM = DEVLIN.DOCNUM
+WHERE
+  NF.PSTDAT BETWEEN '2025-05-29' AND '2025-05-31'
+  AND DEV.PSTDAT BETWEEN '2025-06-01' AND '2025-06-03'
+  AND DEV.NFENUM IS NOT NULL
+  AND DEVLIN.MATNR = LIN.MATNR
