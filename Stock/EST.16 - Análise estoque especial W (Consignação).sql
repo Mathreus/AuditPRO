d8ef44a14f785748dccc3cd7b84885ae6b00684a
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-06-25 09:09:53 -0300

    Update EST.16 - Análise estoque especial W (Consignação).sql

--- Stock/EST.16 - Análise estoque especial W (Consignação).sql
+++ Stock/EST.16 - Análise estoque especial W (Consignação).sql
@@ -2,14 +2,14 @@ SELECT
   W.WERKS AS Centro,
   W.ERSDA AS Data_Criacao,
   W.KUNNR AS ID_Cliente,
-  W.MATNR AS Codigo_Material,
-  W.MAKTX AS Texto_Breve_Material,
+  RIGHT(W.MATNR, 6) AS Codigo_Material,
+  MAT.MAKTX AS Texto_Breve_Material,
   W.KULAB AS QTD
 FROM 
-  `production-servers-magnumtires.prdmgm_sap_cdc_processed.msku` AS W -- Inclusão da tabela solicitada para Washington
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.nsdm_v_msku` AS W 
 INNER JOIN
 	`production-servers-magnumtires.prdmgm_sap_cdc_processed.makt` AS MAT 
-	ON est.mandt = mat.mandt
-	AND RIGHT(EST.MATNR, 6) = RIGHT(MAT.MATNR, 6)
+	ON RIGHT(W.MATNR, 6) = RIGHT(MAT.MATNR, 6)
 WHERE
   W.ERSDA BETWEEN '2025-01-01' AND '2025-05-31'
+  AND W.KULAB <> 0
