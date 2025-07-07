5f14b8b46dd80dcd1b04c6ebc290eb0ce497effa
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-07-07 09:15:10 -0300

    Update DEP.03 - Pagamento frequente para um mesmo RPA.sql

--- Financial/DEP.03 - Pagamento frequente para um mesmo RPA.sql
+++ Financial/DEP.03 - Pagamento frequente para um mesmo RPA.sql
@@ -4,14 +4,15 @@ SELECT
   FF.POSTING_NUMBER AS Documento,
   CASE
     WHEN FF.TRANSACT_TYPE = 'C' THEN 'Entrada caixa de Conta Bancária'
-    WHEN FF.TRANSACT_TYPE = 'B' THEN 'Saída caixa de Conta Bancária'
+    WHEN FF.TRANSACT_TYPE = 'B' THEN 'Saida caixa de Conta Bancária'
     WHEN FF.TRANSACT_TYPE = 'R' THEN 'Receitas'
     WHEN FF.TRANSACT_TYPE = 'E' THEN 'Despesas'
-    WHEN FF.TRANSACT_TYPE = 'D' THEN 'Lançamento Cliente'
-    WHEN FF.TRANSACT_TYPE = 'K' THEN 'Lançamento Fornecedor'
+    WHEN FF.TRANSACT_TYPE = 'D' THEN 'Lancamento Cliente'
+    WHEN FF.TRANSACT_TYPE = 'K' THEN 'Lancamento Fornecedor'
     ELSE 'Nada'
   END Transacao,
 --  FF.TRANSACT_NUMBER AS Tipo_Transacao,
+  FF.VENDOR_NO AS Cod_Fornecedor,
   FF.P_PAYMENTS AS Montante,
   FF.POSITION_TEXT AS Texto
 FROM `production-servers-magnumtires.prdmgm_sap_cdc_processed.tcj_positions` as FF
