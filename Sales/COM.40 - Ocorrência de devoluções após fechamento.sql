a79dd7e2d96152d35860c1345cee0d36695bc861
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-07-14 11:10:42 -0300

    Update COM.40 - Ocorrência de devoluções após fechamento.sql

--- Sales/COM.40 - Ocorrência de devoluções após fechamento.sql
+++ Sales/COM.40 - Ocorrência de devoluções após fechamento.sql
@@ -1,16 +1,19 @@
 SELECT DISTINCT
   LIN.BWKEY AS Centro,
   NF.PSTDAT AS Data_NF,
+  DEV.PSTDAT AS Data_Devolucao,
   NF.PARID AS ID_Externo,
   NF.NAME1 AS Cliente,
   NF.NFENUM AS NF_Faturada,
   RIGHT(LIN.MATNR, 6) AS Material_Faturamento,
   LIN.MAKTX AS Descricao_Material_Faturamento,
-  NF.NFTOT AS Valor_Faturamento,
-  DEV.PSTDAT AS Data_Devolucao,
+  LIN.MENGE AS Quantidade,
+  LIN.NETWR AS Valor_Produto,
+  NF.NFTOT AS Valor_Total_NF,
   DEV.NFENUM AS NF_Devolucao,
   RIGHT(DEVLIN.MATNR, 6) AS Material_Devolvido,
   DEVLIN.MAKTX AS Descricao_Material_Devolvido,
+  DEVLIN.NETWR AS Valor_Produto_Devolucao,
   DEV.NFTOT AS Valor_Devolucao
 FROM    
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` AS NF
@@ -19,14 +22,15 @@ INNER JOIN
   ON NF.DOCNUM = LIN.DOCNUM 
 LEFT JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` AS DEV
-  ON NF.PARID = DEV.PARID  -- Mesmos clientes
-  AND NF.NAME1 = DEV.NAME1 -- Mesmo nome de cliente
-  AND DEV.NFTYPE IN ('ZD', 'Y2') -- Tipos de nota fiscal que indicam devolução
+  ON NF.PARID = DEV.PARID  
+  AND NF.NAME1 = DEV.NAME1 
+  AND DEV.NFTYPE IN ('ZD', 'Y2') 
 LEFT JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnflin` AS DEVLIN
   ON DEV.DOCNUM = DEVLIN.DOCNUM
 WHERE
-  NF.PSTDAT BETWEEN '2025-05-29' AND '2025-05-31'
-  AND DEV.PSTDAT BETWEEN '2025-06-01' AND '2025-06-03'
+  NF.PSTDAT BETWEEN '2025-06-23' AND '2025-06-30'
+  AND DEV.PSTDAT BETWEEN '2025-07-01' AND '2025-07-07'
+  AND NF.PARID > '1000000000'
   AND DEV.NFENUM IS NOT NULL
   AND DEVLIN.MATNR = LIN.MATNR
