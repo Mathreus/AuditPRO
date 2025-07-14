1593e996fdee2c8f286d798a39bb4006e5b950ee
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-07-14 17:35:05 -0300

    Update DEP.03 - Pagamento frequente para um mesmo RPA.sql

--- Financial/DEP.03 - Pagamento frequente para um mesmo RPA.sql
+++ Financial/DEP.03 - Pagamento frequente para um mesmo RPA.sql
@@ -1,7 +1,10 @@
 SELECT
   FF.CAJO_NUMBER AS Centro,
   FF.POSTING_DATE AS Data_Lcto,
+  SEG.AUGCP AS Data_Compensacao,
   FF.POSTING_NUMBER AS Documento,
+  SEG.BELNR AS Lcto,
+  SEG.AUGBL AS DocCompensacao,
   CASE
     WHEN FF.TRANSACT_TYPE = 'C' THEN 'Entrada caixa de Conta Bancária'
     WHEN FF.TRANSACT_TYPE = 'B' THEN 'Saida caixa de Conta Bancária'
@@ -18,7 +21,23 @@ SELECT
   FF.POSITION_TEXT AS Texto
 FROM 
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.tcj_positions` AS FF
-INNER JOIN
+LEFT JOIN 
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.tcj_documents` AS TDOC
+  ON FF.CAJO_NUMBER = TDOC.CAJO_NUMBER
+  AND FF.POSTING_DATE = TDOC.POSTING_DATE
+  AND FF.POSTING_NUMBER = TDOC.POSTING_NUMBER
+LEFT JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.bseg` AS SEG
+  ON FF.COMP_CODE = SEG.BUKRS
+  AND FF.GL_ACCOUNT = SEG.HKONT
+  AND FF.POSTING_DATE = SEG.H_BUDAT
+  AND FF.P_PAYMENTS = SEG.DMBTR
+LEFT JOIN
+  `production-servers-magnumtires.prdmgm_sap_cdc_processed.bkpf` AS BK
+  ON SEG.BELNR = BK.BELNR
+  AND SEG.BUKRS = BK.BUKRS
+  AND SEG.H_BUDAT = BK.BUDAT
+LEFT JOIN
   `production-servers-magnumtires.prdmgm_sap_cdc_processed.lfa1` AS FNC
   ON FF.VENDOR_NO = FNC.LIFNR
 WHERE
