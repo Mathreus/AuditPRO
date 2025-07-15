e3be3a43aefc6701e903e0210fc3fce1af156487
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-07-15 11:46:55 -0300

    Update COM.40 - Ocorrência de devoluções após fechamento.sql

--- Sales/COM.40 - Ocorrência de devoluções após fechamento.sql
+++ Sales/COM.40 - Ocorrência de devoluções após fechamento.sql
@@ -4,12 +4,14 @@ SELECT DISTINCT
   DEV.PSTDAT AS Data_Devolucao,
   NF.PARID AS ID_Externo,
   NF.NAME1 AS Cliente,
+  PED.VBELN AS Pedido,
   NF.NFENUM AS NF_Faturada,
   RIGHT(LIN.MATNR, 6) AS Material_Faturamento,
   LIN.MAKTX AS Descricao_Material_Faturamento,
   LIN.MENGE AS Quantidade_NF,
   LIN.NETWR AS Valor_Produto,
   NF.NFTOT AS Valor_Total_NF,
+  PED.VBELN AS Pedido_Devolucao,
   DEV.NFENUM AS NF_Devolucao,
   RIGHT(DEVLIN.MATNR, 6) AS Material_Devolvido,
   DEVLIN.MAKTX AS Descricao_Material_Devolvido,
@@ -17,7 +19,7 @@ SELECT DISTINCT
   DEVLIN.NETWR AS Valor_Produto_Devolucao,
   DEV.NFTOT AS Valor_Devolucao,
   RELV.PARTNER AS Cod_orig_vend, 
-	concat(vend.bu_sort2,' ',vend.bu_sort1) as Vendedor,
+  CONCAT(vend.bu_sort2,' ',vend.bu_sort1) as Vendedor,
   (DEVLIN.MENGE - LIN.MENGE) AS DIF_QTD,
   (DEVLIN.NETWR - LIN.NETWR) AS DIF_VALOR_PRODUTO,
   (DEV.NFTOT - NF.NFTOT) AS DIF_NF
@@ -45,13 +47,13 @@ LEFT JOIN
   ON ITEM.MANDT = PED.MANDT  
   AND ITEM.VBELN = PED.VBELN 
 LEFT JOIN 
-	`production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` AS RELV 
-	ON ped.mandt = relv.client 
-	AND relv.idnumber = item.perve_ana
+   `production-servers-magnumtires.prdmgm_sap_cdc_processed.but0id` AS RELV 
+    ON ped.mandt = relv.client 
+    AND relv.idnumber = item.perve_ana
 LEFT JOIN
-	`production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` AS VEND 
-	ON ped.mandt = vend.client 
-	AND relv.partner = vend.partner
+    `production-servers-magnumtires.prdmgm_sap_cdc_processed.but000` AS VEND 
+     ON ped.mandt = vend.client 
+     AND relv.partner = vend.partner
 WHERE
   NF.PSTDAT BETWEEN '2025-06-23' AND '2025-06-30'
   AND DEV.PSTDAT BETWEEN '2025-07-01' AND '2025-07-07'
