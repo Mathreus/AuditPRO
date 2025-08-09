afd503b63d9ccbe120a2b1cc47f762fc10d41526
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-08-09 18:52:59 -0300

    Update COM.02 - Venda para CNPJ Inativos.sql

--- Sales/COM.02 - Venda para CNPJ Inativos.sql
+++ Sales/COM.02 - Venda para CNPJ Inativos.sql
@@ -1,14 +1,29 @@
-SELECT 
-	DOC.DOCNUM AS 'NUM_DOC', 
-	DOC.PARID AS 'COD_CLIENTE',
-	DOC.NAME1 AS 'CLIENTE',
-	DOC.CGC AS 'CNPJ',
-	DOC.DOCDAT AS 'DT_DOC',
-	DOC.NFENUM AS 'NUM_NF', 
-	DOC.NFTOT AS 'VLR_NF'
-FROM 
-	`production-servers-magnumtires.prdmgm_sap_cdc_processed.j_1bnfdoc` AS DOC
-WHERE
-	DOC.DOCDAT BETWEEN '2024-01-01' AND '2024-06-30' AND
-	DOC.CGC <> '00000000000000' AND
-	DOC.PARID > '1000000000'
+import requests
+import json
+import pandas as pd
+import time
+
+df1 = pd.read_excel(r'caminho\arquivo.xlsx')
+df2 = df1['CNPJ']
+df3 = (pd.DataFrame({'CNPJ': [], 'SITUACAO': []}))
+
+def consulta_cnpj(cnpj):
+    url = f'https://www.receitaws.com.br/v1/cnpj/{cnpj}'
+    querystring = {"token": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", "cnpj": "06990590000123", "plugin": "RF"}
+    response = requests.request('GET', url, params=querystring)
+
+    resp = json.loads(response.text)
+    time.sleep(30)
+
+    # Me retorna apenas os campos de meu interesse
+    return resp['situacao']
+
+# print((df1['CNPJ'].iloc[0]))
+
+for i in range(len(df2)):
+    # print(df1['CNPJ'].iloc[i])
+    try:
+        print(df1['CNPJ'].iloc[i], "|", consulta_cnpj(df1['CNPJ'].iloc[i]))
+
+    except KeyError:
+        print(df1['CNPJ'].iloc[i])
